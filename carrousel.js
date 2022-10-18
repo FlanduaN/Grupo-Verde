2b46fdf0a454443fe319663a500414885d5309a9
FlanduaN <nicolas.flandua@gmail.com>
2022-10-18 18:34:03 -0300

    Carrousel

---
+++ carrousel.js
@@ -0,0 +1,19 @@
+
+const carrousel = document.querySelector('.carrousel')
+const selector = document.querySelectorAll('.selector')
+
+selector.forEach( (cadaSelector , i ) => {
+    selector[i].addEventListener('click',()=>{
+
+        let posicion = i
+        let operacion = posicion * -25
+
+        carrousel.style.transform = `translateX(${ operacion }%)`
+
+        selector.forEach(( cadaSelector , i )=>{
+            selector[i].classList.remove('activo')
+        })
+        selector[i].classList.add('activo')
+        
+    })
+});
